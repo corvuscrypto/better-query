02f9abdee1a0a7f657a3ab2195af92a5836f2e7c
Clifford Richardson <clifford.richardson.@fyndiq.com>
2018-03-21 10:20:28 +0100

    Added package setup and gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bigquery_sucks',
+    version='0.1.0',
+    packages=find_packages(exclude=['tests*', 'examples']),
+    description='Maybe a better client? Maybe not. Either way BigQuery sucks',
+    author='Clifford Richardson',
+    author_email='cmrallen@gmail.com',
+    install_requires=["google-auth==1.4.1"],
+)
